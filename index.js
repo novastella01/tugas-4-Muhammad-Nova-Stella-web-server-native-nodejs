ce12896036bb305298e202204586289c57829059
Muhammad Nova Stella <122338377+novastella01@users.noreply.github.com>
2023-09-17 00:24:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const boot = require("./src/server");
 
 const server = boot()
 const port = 3000
-server.listen(port,() => {
+server.listen(port, () => {
     console.log(`Server run in http://localhost:${port}`);
 })
