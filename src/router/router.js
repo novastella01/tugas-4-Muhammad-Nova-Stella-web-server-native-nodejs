881b77f080277ee3ef91e3d839a45d28444a5103
Muhammad Nova Stella <122338377+novastella01@users.noreply.github.com>
2023-09-17 00:25:51 +0700

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,14 +1,21 @@
-const postHandler = require("../handler/postHandler")
+const postHandler = require("../handler/postHandler.js")
+const commentHandler = require("../handler/commentHandler.js")
+const postcommentHandler = require("../handler/postcommentHandler.js")
+
+
 
 const router = {}
-router.init = (req,res) => {
-    if(req.url === "/api/post/get"){
-        postHandler.getAllPost(req,res)
+router.init = (req, res) => {
+
+    if (req.url === "/api/post/get") {
+        postHandler.getAllPost(req, res);
     }
-
     // silahkan tambahkan routing lain disini
-
-    else {
+    else if (req.url === "/api/comment/get") {
+        commentHandler.getAllComments(req, res);
+    } else if (req.url === "/api/post-comment/get") {
+        postcommentHandler.getAllPostComments(req, res);
+    } else {
         res.end("Not Found Route !")
     }
 }
